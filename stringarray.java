079d41d5e325aa7caa43d6c4c7257d2bf1c750bc
broli <carlos.ferrabone@gmail.com>
2010-06-28 20:51:09 -0300

    A few ideas the book game to test arrays

---
+++ stringarray.java
@@ -0,0 +1,11 @@
+
+public class stringarray {
+	public static void main (String[] args){
+
+	//para probar lo del "hypen"
+	String[] lista = { "primero", "segun- do","tercero" };
+
+	System.out.print(lista[1]);
+
+	}
+}
