a0325dbccc806f0e2f3f5438e3a567c4c2797693
broli <carlos.ferrabone@gmail.com>
2010-06-28 20:50:38 -0300

    Dog class and test class

---
+++ dog.java
@@ -0,0 +1,12 @@
+
+class dog {
+	
+	int size;
+	String breed;
+	String name;
+
+	void bark() {
+		System.out.println("wuff wuff");
+	}
+
+}
---
+++ dogTestDrive.java
@@ -0,0 +1,11 @@
+class dogTestDrive {
+	
+	public static void main (String[] args) {
+		//test
+
+		dog hunter = new dog();
+		hunter.size = 40;
+		hunter.bark();
+	}
+
+}
