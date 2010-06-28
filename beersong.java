de91e22de62d72e40ec658aa13df174b474e5130
broli <carlos.ferrabone@gmail.com>
2010-06-28 20:50:02 -0300

    App that sings the beersong

---
+++ beersong.java
@@ -0,0 +1,15 @@
+
+
+public class beersong {
+	public static void main (String[] args) {
+		int beers = 99;
+		while ( beers > 1 ) {
+			System.out.println( beers + " bottles of beer on the wall," + beers +" bottles of beer.");
+			beers--;
+			System.out.println( "Take one down, pass it around, " + beers + " bottles of beer on the wall.");
+
+		}
+
+	}
+
+}
