2dba3ab6149693c2c4654acab04bdc2c7e109ea5
broli <carlos.ferrabone@gmail.com>
2010-06-30 14:20:09 -0300

    book examples, more code and some LF/CRLF stupidity

---
+++ DotComGame/simple/SinkaDotcom.java
@@ -0,0 +1,31 @@
+
+public class SinkaDotcom {
+	
+	public static void main (String[] args) {
+		
+		GameHelper helper = new GameHelper();
+		DotCom DotCom1 = new DotCom();
+		String entrada;
+		boolean resultado;
+	
+		System.out.println("Empezamos el juego!");
+		System.out.println("ingrese una posicion");
+		
+		DotCom1.startDotCom();
+
+		while (!DotCom1.isDead()) {
+			entrada = helper.getUserInput("Adivina: ");	
+			
+			resultado = DotCom1.checkHit(Integer.parseInt(entrada));
+			if (resultado) {
+				System.out.println("|X| Pego!!!");
+			} else {
+				System.out.println("| | NO Pego!!!");
+			}
+
+		}
+
+		System.out.println("Hundido!!! ");
+
+	}
+}
