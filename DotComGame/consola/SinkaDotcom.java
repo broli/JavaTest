c0877e387594671769f009f6c3b0a208c467502a
broli <carlos.ferrabone@gmail.com>
2010-07-01 23:50:41 -0300

    finished the main game class
    lots of bugs. little details that donr work quite as spected
    but dont want to refactor half of the code. not now anyways. the book
    probably will return to this thing

--- DotComGame/consola/SinkaDotcom.java
+++ DotComGame/consola/SinkaDotcom.java
@@ -3,24 +3,54 @@ import java.util.*;
 public class SinkaDotcom {
 	
 	private GameHelper helper = new GameHelper();
-	private ArrayList<DotCom> dotComsList = new AIrayList<DotCom>();
+	private ArrayList<DotCom> dotComsList = new ArrayList<DotCom>();
 	private int numOfGuesses = 0;
 
-	private void setUpGame() {
-		Dotcom tmptrash = new DotCom(); //create obj
+	public void setUpGame() {
+		DotCom tmptrash = new DotCom(); //create obj
 		tmptrash.setName("pets.com"); //Set name
 		tmptrash.startDotCom();
-		dotComList.add(tmptrash); //Add obj to the list
+		dotComsList.add(tmptrash); //Add obj to the list
 
-		Dotcom tmptrash = new DotCom(); //create obj
+		tmptrash = new DotCom(); //create obj
 		tmptrash.setName("toys.com"); //Set name
 		tmptrash.startDotCom();
-		dotComList.add(tmptrash); //Add obj to the list
+		dotComsList.add(tmptrash); //Add obj to the list
 
-		Dotcom tmptrash = new DotCom(); //create obj
+		tmptrash = new DotCom(); //create obj
 		tmptrash.setName("yahoo.com"); //Set name
 		tmptrash.startDotCom();
-		dotComList.add(tmptrash); //Add obj to the list
+		dotComsList.add(tmptrash); //Add obj to the list
 
 	}
+
+	public void startPlaying () {
+		while(!dotComsList.isEmpty() ){
+			String userGuess = helper.getUserInput("Enter a guess");
+			checkUserGuess(userGuess);
+		}
+		finishGame ();
+	}
+	
+	private void checkUserGuess(String userGuess) {
+		numOfGuesses++;
+
+		for (DotCom puntero : dotComsList) {
+			if (puntero.checkHit(userGuess) == true ) {
+				System.out.println(puntero.getName() + "\t|x|");
+				if (puntero.isDead()) {
+					System.out.println("You sinked " + puntero.getName() );
+					dotComsList.remove(puntero);
+				}
+				break;
+			}else {
+				System.out.println(puntero.getName() +  "\t| |");
+			}
+		}
+	}
+
+	private void finishGame() {
+		System.out.println("Cantidad de intentos: " + numOfGuesses);
+	}
+
 }
